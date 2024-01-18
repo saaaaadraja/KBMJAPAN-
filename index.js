e15fd99287bfef424f3cac83c3022ff14296bf48
rajasaad <rmsik92@gmail.com>
2024-01-18 18:02:59 +0500

    changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 const multer  = require('multer');
